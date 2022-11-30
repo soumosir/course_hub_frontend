afa9f09c346a2c48f6dfeb8cdd9cca903418f83c
Soumosir Dutta <sdutta7@umd.edu>
2022-11-30 18:46:33 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Home from './components/home/home'
 import Courses from './components/courses/courses'
 import Wishlist from './components/wishlist/wishlist'
 import CourseDetail from './components/courseDetail/courseDetail'
+import CourseAdd from './components/courseAdd/courseAdd'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,6 +23,7 @@ function App() {
         <Route path='/wishlist' element={ <Wishlist/> }/>
         <Route path='/course/:id' element={<CourseDetail/> }/>
         <Route path='/signin' element={<UserSignIn/> }/>
+        <Route path='/courseadd' element={<CourseAdd/> }/>
         {/* <Route path='/' element={ </> }/> */}
         {/* <Route path='/signup' element={ <UserSignUp/> }/> */}
 
