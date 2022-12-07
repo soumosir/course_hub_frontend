ca0099a24cc1073239e2b2d55ccc967b12d7fd28
Junaid Zaidi <junaidzaidi@wireless-10-104-8-31.umd.edu>
2022-12-07 15:24:04 -0500

    signup signin navigation

--- src/components/userSignIn/userSignIn.tsx
+++ src/components/userSignIn/userSignIn.tsx
@@ -140,7 +140,7 @@ export default function UserSignIn() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
