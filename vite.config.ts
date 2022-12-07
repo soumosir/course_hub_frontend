848b913cad20763a8602703f0ca1b2055e0f0a8f
Somesh Mehta <seagel@wireless-10-104-67-209.umd.edu>
2022-12-07 18:07:45 -0500

    testing for soumosit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server : {
-    port : 5000,
-  },
-  plugins: [react()]
-})
+
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return defineConfig({
+    plugins: [react()],
+    server : {
+      port : process.env.PORT || 3000
+    }
+  });
+}
